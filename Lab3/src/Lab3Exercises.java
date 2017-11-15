12fe33cc4ad27071dfdaac0c2ba4c5ce9f04553a
4015c3 <4015c3@guama.ece.uprm.edu>
2017-11-15 16:23:31 -0400

    Made the window appear in the center of my screen.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,8 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Gabriel Gonzalez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(1920/2 - 400/2, 1080/2 - 100/2);
-		myFrame.setSize(400, 100);
+		myFrame.setLocation(1920/2 - 200/2, 1080/2 - 200/2);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
